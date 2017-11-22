2d0b815c532fd4cae084a3360b76ec0964cd9d5e
Pierre <pierre.nortje@beingit.co.za>
2017-11-22 20:21:41 +0200

    Removed unused dependencies. Added test classes.

---
+++ Zomato API/Zomato.API.Tests/CommonRequestTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zomato.API.Tests
+{
+    [TestClass]
+    public class CommonRequestTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Zomato API/Zomato.API.Tests/LocationRequestTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zomato.API.Tests
+{
+    [TestClass]
+    public class LocationRequestTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
